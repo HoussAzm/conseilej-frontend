8f9cf5e7012fe0f5a4a44b367bd100b306f259ed
HoussAzm <houssamazmour@gmail.com>
2024-03-06 13:48:54 +0100

    Ajouter des titres pour SEO

--- src/components/Home/CarouselItem.tsx
+++ src/components/Home/CarouselItem.tsx
@@ -24,7 +24,7 @@ const CarouselItem: React.FC<CarouselItemProps> = ({ title, subTitle, image }) =
 
   return (
     <div className="carousel-item" style={itemStyle}>
-      <h2 className="text-3xl font-bold text-gray-800">{title}</h2>
+      <h2 className="text-3xl font-bold text-gray-800" >{title}</h2>
       <p className="text-lg mt-4 font-bold text-gray-800">{subTitle}</p>
     </div>
   );
