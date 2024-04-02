7a902244d3fa0697331230a1bc3b5dd950456784
HoussAzm <houssamazmour@gmail.com>
2024-04-02 18:19:13 +0200

    delete espace

--- src/components/Description/ChewNous.tsx
+++ src/components/Description/ChewNous.tsx
@@ -5,7 +5,7 @@ const ChewNous = () => {
       <div className="md:w-11/12 mx-auto flex flex-col md:flex-row justify-between items-center gap-12">
         <div>
           <video controls autoPlay>
-            <source src={presentation} type="video/mp4"/>
+            <source src={presentation} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
@@ -14,15 +14,13 @@ const ChewNous = () => {
             CONSEIL EJ
           </h2>
           <p className="text-md font-semibold mb-8 text-justify">
-          Bienvenue chez Conseil EJ, votre partenaire de confiance pour une gestion patrimoniale exceptionnelle.
-           Spécialisés dans le conseil patrimonial, l'immobilier de prestige, la gestion patrimoniale, le conseil
-            financier et l'investissement stratégique, nous nous engageons à vous offrir une expérience personnalisée 
+            Bienvenue chez Conseil EJ, votre partenaire de confiance pour une gestion patrimoniale exceptionnelle.
+            Spécialisés dans le conseil patrimonial, l'immobilier de prestige, la gestion patrimoniale, le conseil
+            financier et l'investissement stratégique, nous nous engageons à vous offrir une expérience personnalisée
             et enrichissante.
-
-          Chez Conseil EJ, nous comprenons l'importance de la planification patrimoniale dans la réalisation
-           de vos objectifs financiers. Notre équipe dédiée de professionnels chevronnés vous accompagne à chaque étape,
+            Chez Conseil EJ, nous comprenons l'importance de la planification patrimoniale dans la réalisation
+            de vos objectifs financiers. Notre équipe dédiée de professionnels chevronnés vous accompagne à chaque étape,
             offrant des conseils avisés pour maximiser vos actifs et assurer la croissance de votre patrimoine.
-
             La gestion patrimoniale est au cœur de notre mission, et nous nous efforçons de créer des stratégies sur mesure adaptées à vos besoins spécifiques. Nos conseils financiers éclairés visent à optimiser vos ressources et à sécuriser votre avenir financier.
           </p>
         </div>
